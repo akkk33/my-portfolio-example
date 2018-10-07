0ba989b64d7db047154ff5c37c17c841c1cb7a30
Amr <akkk33@protonmail.com>
2018-10-07 09:20:35 +0200

    done event listener with javascript

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,30 @@
-const menu_btn = document.getElementsByClassName("menu-btn");
-const menu = document.getElementsByClassName("menu");
-const menu_branding = document.getElementsByClassName("menu-branding");
-const menu_nav = document.getElementsByClassName("menu-nav");
-const nav_item = document.getElementsByClassName("nav-item");
+const menu_btn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menu_branding = document.querySelector(".menu-branding");
+const menu_nav = document.querySelector(".menu-nav");
+const nav_item = document.querySelectorAll(".nav-item");
 
 let menu_status = false;
+
+menu_btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!menu_status) {
+
+        menu_btn.classList.add("close");
+        menu.classList.add("show");
+        menu_branding.classList.add("show");
+        menu_nav.classList.add("show");
+        nav_item.forEach(item => item.classList.add('show'));
+
+        menu_status = true;
+    } else {
+        menu_btn.classList.remove("close");
+        menu.classList.remove("show");
+        menu_branding.classList.remove("show");
+        menu_nav.classList.remove("show");
+        nav_item.forEach(item => item.classList.remove('show'));
+
+        menu_status = false;
+    }
+}
