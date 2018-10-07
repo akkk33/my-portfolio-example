3dc2706f35707e33b116b3f0513e1916410209b8
Amr <akkk33@protonmail.com>
2018-10-07 08:59:31 +0200

    playing with javascript

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+const menu_btn = document.getElementsByClassName("menu-btn");
+const menu = document.getElementsByClassName("menu");
+const menu_branding = document.getElementsByClassName("menu-branding");
+const menu_nav = document.getElementsByClassName("menu-nav");
+const nav_item = document.getElementsByClassName("nav-item");
+
+let menu_status = false;
